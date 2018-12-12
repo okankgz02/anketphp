7384906c3b644fd6cc56f69fd2473831bc35d372
okankgz02 <okankgz@gmail.com>
2018-12-12 19:53:54 +0300

    project

---
+++ nedmin/netting/baglan.php
@@ -0,0 +1,21 @@
+﻿
+<?php 
+
+
+try {
+
+
+	$db=new PDO("mysql:host=localhost;dbname=shopalis_anket;charset=utf8",'shopalis_anket','okanokan02');
+
+/*	echo "başarılı";
+*/
+} catch (PDOExpception $e) {
+
+
+	echo $e->getMessage();
+
+}
+
+require_once 'class.phpmailer.php';
+
+ ?>
